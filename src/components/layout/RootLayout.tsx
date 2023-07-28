c5fa18ce73412642b4151dd68ef384bf4e94d970
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2023-07-28 12:19:52 +0600

    [DONE] ready to start

---
+++ src/components/layout/RootLayout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+interface RootLayoutProps {
+  children: React.ReactNode; // Use ReactNode type for children prop
+}
+
+const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <div style={{ padding: "0 50px" }}>
+        <div>{children}</div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default RootLayout;
