1a0cda42188f165dbbbcffd62b96a9cf98841494
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2023-07-28 12:28:06 +0600

    [DONE] ready to start 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,13 @@
-import RootLayout from "@/components/layout/RootLayout";
 import { Inter } from "next/font/google";
-import { ReactElement } from "react";
-import type { NextPageWithLayout } from "./_app";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const Home: NextPageWithLayout = () => {
+const Home = () => {
   return (
     <div className="mx-auto w-full max-w-screen-xl">
       <h1>this is me</h1>
     </div>
   );
-};
-
-Home.getLayout = function getLayout(page: ReactElement) {
-  return <RootLayout>{page}</RootLayout>;
 };
 
 export default Home;
